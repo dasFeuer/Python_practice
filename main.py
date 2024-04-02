fb10dae1aa4db4621d9df8fc519b17fbdc9c512c
dasFeuer <barunpanthisharma11@gmail.com>
2024-04-02 21:16:17 +0545

    new file added

---
+++ main.py
@@ -0,0 +1,13 @@
+# For loop
+
+num = int(input("Enter the number: "))
+for i in range(20):
+    print(num, "X", i + 1 , "=", num * (i + 1))
+
+# While Loop
+    
+num = int(input("Enter the number: "))
+i = 1
+while (i <= 20):
+    print(num, "X", i , "=", num * i)
+    i = i + 1
