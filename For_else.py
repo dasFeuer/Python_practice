70b1311f8c048bdee6ef6da3098c2f48b725f40c
dasFeuer <barunpanthisharma11@gmail.com>
2024-04-08 07:56:04 +0545

    New files added

---
+++ For_else.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    print(i)
+
+else:
+    print("Sorry NO!")
+
+for x in range(5):
+    print("Iteration no {} in for loop".format(x+1))
+else:
+    print("Else block in loop")
+print("Out of loop")
