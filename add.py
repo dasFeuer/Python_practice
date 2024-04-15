02376eca934ae575757b1e3df5b5ce8e02b99e85
dasFeuer <barunpanthisharma11@gmail.com>
2024-04-15 08:01:20 +0545

    new file added

---
+++ add.py
@@ -0,0 +1,34 @@
+import operator
+
+num1 = 1
+num2 = 2
+
+sum = operator.add(num1,num2)
+print(f"sum of {num1} and {num2} is {sum}")
+
+
+addNum = lambda x, y: x + y
+
+num1 = 4
+num2 = 6
+
+result = addNum(num1, num2)
+
+print(f"sum of {num1} and {num2} is {result}")
+
+
+def addNumRecursive (c, d):
+    if d == 0:
+        return c
+    else:
+        return addNumRecursive(c + 1, d -1)
+
+num1 = int(input("Enter 1st number:"))
+num2 = int(input("Enter 2nd number:"))
+
+res = addNumRecursive(num1, num2)
+
+print(f"sum of {num1} and {num2} is {res}")
+
+
+
